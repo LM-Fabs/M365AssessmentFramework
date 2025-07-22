06a7c9a4a4058bfa772cdb94e893d363a1fbfab1
LM-Fabs <fabian.sodke@lm-ag.de>
2025-07-22 15:17:26 +0200

    Fix API deployment: Correct handler paths for Azure Functions v4 recognition

--- m365-security-assessment/m365-assessment-framework/api/index.ts
+++ m365-security-assessment/m365-assessment-framework/api/index.ts
@@ -7,21 +7,21 @@ app.http('test-simple', {
     methods: ['GET', 'POST', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous',
     route: 'test-simple',
-    handler: require('./test-simple').default
+    handler: require('./test-simple/index').default
 });
 
 app.http('hello', {
     methods: ['GET', 'POST', 'HEAD', 'OPTIONS'], 
     authLevel: 'anonymous',
     route: 'hello',
-    handler: require('./hello').default
+    handler: require('./hello/index').default
 });
 
 app.http('diagnostics', {
     methods: ['GET', 'POST', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous', 
     route: 'diagnostics',
-    handler: require('./diagnostics').default
+    handler: require('./diagnostics/index').default
 });
 
 // Customer Management Functions
@@ -29,14 +29,14 @@ app.http('customers', {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous',
     route: 'customers/{*route?}',
-    handler: require('./customers').default
+    handler: require('./customers/index').default
 });
 
 app.http('customerById', {
     methods: ['GET', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous',
     route: 'customers/{customerId}',
-    handler: require('./customerById').default
+    handler: require('./customerById/index').default
 });
 
 // Assessment Functions  
@@ -44,28 +44,28 @@ app.http('assessments', {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous',
     route: 'assessments/{*route?}',
-    handler: require('./assessments').default
+    handler: require('./assessments/index').default
 });
 
 app.http('currentAssessment', {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous',
     route: 'assessments/current/{*route?}',
-    handler: require('./currentAssessment').default
+    handler: require('./currentAssessment/index').default
 });
 
 app.http('customerAssessments', {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous',
     route: 'customers/{customerId}/assessments/{*route?}',
-    handler: require('./customerAssessments').default
+    handler: require('./customerAssessments/index').default
 });
 
 app.http('createAssessment', {
     methods: ['GET', 'POST', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous',
     route: 'assessments/create/{*route?}',
-    handler: require('./createAssessment').default
+    handler: require('./createAssessment/index').default
 });
 
 // Best Practices Functions
@@ -73,14 +73,14 @@ app.http('best-practices', {
     methods: ['GET', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous',
     route: 'best-practices/{*route?}',
-    handler: require('./best-practices').default
+    handler: require('./best-practices/index').default
 });
 
 app.http('bestPractices', {
     methods: ['GET', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous',
     route: 'bestPractices/{*route?}',
-    handler: require('./bestPractices').default
+    handler: require('./bestPractices/index').default
 });
 
 // Authentication Functions
@@ -88,14 +88,14 @@ app.http('consent-callback', {
     methods: ['GET', 'POST', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous',
     route: 'consent-callback/{*route?}',
-    handler: require('./consent-callback').default
+    handler: require('./consent-callback/index').default
 });
 
 app.http('enterprise-app', {
     methods: ['GET', 'POST', 'PUT', 'DELETE', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous',
     route: 'enterprise-app/{*route?}',
-    handler: require('./enterprise-app').default
+    handler: require('./enterprise-app/index').default
 });
 
 // Legacy/Test Functions
@@ -103,21 +103,21 @@ app.http('test-function', {
     methods: ['GET', 'POST', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous',
     route: 'test-function',
-    handler: require('./test-function').default
+    handler: require('./test-function/index').default
 });
 
 app.http('HttpTrigger', {
     methods: ['GET', 'POST', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous',
     route: 'HttpTrigger',
-    handler: require('./HttpTrigger').default
+    handler: require('./HttpTrigger/index').default
 });
 
 app.http('test', {
     methods: ['GET', 'POST', 'HEAD', 'OPTIONS'],
     authLevel: 'anonymous',
     route: 'test/{*route?}',
-    handler: require('./test').default
+    handler: require('./test/index').default
 });
 
 console.log('✅ Azure Functions v4 registration completed - All endpoints registered');
